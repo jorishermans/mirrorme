94881e1e4aae48d3ff7488e65a11a9c19c89e2ff
luis <luisbigvargas@gmail.com>
2014-08-16 15:56:25 -0400

    Remove String constanst Change getMirrorValues to from method

--- lib/helpers/force_metadata_helpers.dart
+++ lib/helpers/force_metadata_helpers.dart
@@ -1,49 +1,28 @@
 part of dart_force_mirrors_lib;
 
-class MetaDataHelper<T> {
-  
-  static const String method = "method";
-  static const String variables = "variables";
-  
-  List<MetaDataValue<T>> getMirrorValues(Object obj) {
-   return _mirrorValues(obj, method); 
+/**
+ * Helper class to create [MetadataValue]<[T]> from the Declarations (Variables and Methods)
+ * in the passed obj.
+ */
+class MetaDataHelper<T, DM> {
+  /**
+   * Get the Iterable of [DeclarationMirror] that are annotated with [T]
+   */
+  Iterable<DeclarationMirror> _scan(InstanceMirror im) {
+      return im.type.declarations.values
+            .where((DeclarationMirror dm) => 
+                dm is DM
+                && dm.metadata.any((am) => am.reflectee is T));
   }
-  
-  List<MetaDataValue<T>> getVariableMirrorValues(Object obj) {
-     return _mirrorValues(obj, variables); 
+
+  /**
+   * Get MetaDataValue of the obj's properties annotated with T (ej. @Value or @Autowired)
+   */
+  List<MetaDataValue<T>> from(Object obj) {
+    var instanceMirror = reflect(obj);
+    return new List.from(_scan(instanceMirror).map((dm) {
+      InstanceMirror am1 = dm.metadata.firstWhere((am) => am.reflectee is T);
+      return new MetaDataValue<T>(am1.reflectee, dm, dm.simpleName, instanceMirror);
+    }));
   }
-  
-  List<MetaDataValue<T>> _mirrorValues(Object obj, String type) {
-    InstanceMirror instanceMirror = reflect(obj);
-    ClassMirror MyClassMirror = instanceMirror.type;
-   
-    Iterable<DeclarationMirror> decls =
-        MyClassMirror.declarations.values;
-    
-    List<MetaDataValue> mirrorValues = new List<MetaDataValue>();
-    
-    for (DeclarationMirror dclMirror in decls) {
-      if (dclMirror is MethodMirror && type == method) {
-        _addMirror(mirrorValues, instanceMirror, dclMirror);
-      } else if (dclMirror is VariableMirror && type == variables) {
-        _addMirror(mirrorValues, instanceMirror, dclMirror);
-      }
-    }
-    return mirrorValues;
-  }
-  
-  void _addMirror(List<MetaDataValue> mirrorValues, InstanceMirror instanceMirror, DeclarationMirror dclMirror) {
-    if (dclMirror.metadata.isNotEmpty) {
-           // var request = mm.metadata.first.reflectee;
-           for (var im in dclMirror.metadata) {
-               if (im.reflectee is T) {
-                   var request = im.reflectee;
-                   Symbol memberName = dclMirror.simpleName;
-                          
-                   mirrorValues.add(new MetaDataValue<T>(request, dclMirror, memberName, instanceMirror));
-                }
-            }
-     }
-  }
-  
 }
